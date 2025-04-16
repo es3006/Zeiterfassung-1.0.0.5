7fb3c98e226403398423ded682fc98cd6785a44d
es3006 <es3006@mailbox.org>
2025-04-16 17:14:21 +0200

    v1.0.0.5: Wass fuer eine Ueberraschung Ende

--- _github_push_changes.bat
+++ _github_push_changes.bat
@@ -1,4 +1,4 @@
 cd /d "D:\DEVELOPMENT\Delphi\Projekte\Firma\ESD Zeiterfassung 1.0.0.5"
 git add .
-git commit -m "v1.0.0.5: Überraschung Und Ende" --allow-empty
+git commit -m "v1.0.0.5: Wass fuer eine Ueberraschung Ende" --allow-empty
 git push
