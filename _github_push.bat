481d2391b29c8b1099a0e6579f00bcc9b992ead7
es3006 <es3006@mailbox.org>
2025-04-17 18:24:18 +0200

    1.0.0.5: Ein Programm zum extrahieren der Schichten aller Mitarbeiter aus einer Dienstplan Exceldatei in eine Zeiterfassung Exceldatei

--- _github_push.bat
+++ _github_push.bat
@@ -1,8 +1,8 @@
-echo # Zeiterfassung-1.0.0.5 >> README.md
+echo # ESD-Besuchererfassung-1.0.0.5 >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/es3006/Zeiterfassung-1.0.0.5.git
+git remote add origin https://github.com/es3006/ESD-Besuchererfassung-1.0.0.5.git
 git push -u origin main
 exit /b 0
