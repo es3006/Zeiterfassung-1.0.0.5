97542d7acf15ed2b02b70dcf9d1a832f15e7c13c
es3006 <es3006@mailbox.org>
2025-04-17 16:15:07 +0200

    1.0.0.5: Ein Programm zum extrahieren der Schichten aller Mitarbeiter aus einer Dienstplan Exceldatei in eine Zeiterfassung Exceldatei

--- _github_push_changes.bat
+++ _github_push_changes.bat
@@ -1,4 +1,4 @@
 cd /d "D:\DEVELOPMENT\Delphi\Projekte\Firma\ESD Zeiterfassung 1.0.0.5"
 git add .
-git commit -m "v1.0.0.5: Wass fuer eine Ueberraschung Ende" --allow-empty
+git commit -m "1.0.0.5: Ein Programm zum extrahieren der Schichten aller Mitarbeiter aus einer Dienstplan Exceldatei in eine Zeiterfassung Exceldatei" --allow-empty
 git push
